b4c08f5f4fe9666da3295f542c33400543a0f748
Vini <ixxvinicius@gmail.com>
2023-02-01 19:46:15 -0300

    Organizando conteudos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+
+function Home() {
+  return (
+    <>
+      <h1>Lista de Presença</h1>
+    </>
+  )
+}
+
+export default Home;
