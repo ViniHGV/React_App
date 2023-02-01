b97cd8a41a41760404a2e8cbf6731779002bc93f
Vini <ixxvinicius@gmail.com>
2023-02-01 20:15:39 -0300

    Criando a pasta Styles para a organização do projeto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import reactLogo from '../assets/react.svg';
-import './index.css'
+import '../styles/index.css'
 
 function Home() {
   return (
     <div className='Home'>
       <div class="mb-3">
-      <h1>Lista de Presença</h1>
-      <label for="" class="form-label">Digite o Nome...</label>
-      <input type="text" class="form-control" id=""/>
-  </div>
+        <h1>Lista de Presença</h1>
+        <br/>
+        <input type="text" class="form-control" id="" placeholder='Digite seu Nome:'/>
+        <br/>
+        <button type="button" class="btn btn-success">Adicionar contato</button>
+      </div>
     </div>
   )
 }
