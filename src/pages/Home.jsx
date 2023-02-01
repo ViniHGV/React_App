7bc0f4b120290dc5b01fd54343970a2bb97c3e2c
Vini <ixxvinicius@gmail.com>
2023-02-01 20:07:08 -0300

    estilizando a página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,13 @@ import './index.css'
 
 function Home() {
   return (
-    <>
+    <div className='Home'>
+      <div class="mb-3">
       <h1>Lista de Presença</h1>
-    </>
+      <label for="" class="form-label">Digite o Nome...</label>
+      <input type="text" class="form-control" id=""/>
+  </div>
+    </div>
   )
 }
 
