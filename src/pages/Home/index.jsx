60d29a2c3a052d690a0864fcbe1f9ffa90b1356d
Vini <ixxvinicius@gmail.com>
2023-02-02 11:55:56 -0300

    Adicionando a função handleAddStudent

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,26 +5,27 @@ import Card from '../../components/card';
 
 function Home() {
   const [studentName, setStudentName] = useState();
-
+  const [students, setStudents] = useState([]);
 
   return (
     <div className='container'>
-        <h1>Nome: {studentName}</h1>
-        <input 
-          type="text" 
-          placeholder='Digite seu Nome:'
-          onChange={e => setStudentName(e.target.value)}
-        />
-        <button>Adicionar contato</button>
-        <Card 
-          name="Vinicius Henrique" 
-          time="11:41" 
-        />
-        <Card 
-          name="Nilton Victor" 
-          time="12:25" 
-        />
-      </div>
+      <h1>Lista de Presença</h1>
+      <input
+        type="text"
+        placeholder='Digite seu Nome:'
+        onChange={e => setStudentName(e.target.value)}
+      />
+      <button>Adicionar contato</button>
+
+      {
+        students.map(student => <Card
+          name={student.name}
+          time={student.time}
+        />)
+
+      }
+
+    </div>
   )
 }
 
